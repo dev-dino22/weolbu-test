2270cbfb92a9375313bc2adad00baf60e3f0d5dd
dev-dino22 <wlsskdud02@naver.com>
2025-12-12 00:18:42 +0900

    feat: 스타일 초기화 reset 파일 작성 및 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import reset from './shared/styles/reset.ts';
+import { Global } from '@emotion/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Global styles={reset} />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
