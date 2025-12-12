368bc792cb14374516fdf1bb2abba1799c1c2285
dev-dino22 <wlsskdud02@naver.com>
2025-12-13 04:04:12 +0900

    refactor: 메인페이지 내용 수정

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,4 +1,5 @@
 import Button from '@components/actions/Button';
+import styled from '@emotion/styled';
 import { ROUTE_PATH } from '@routes/routePath';
 import { useNavigate } from 'react-router';
 
@@ -6,13 +7,38 @@ function MainPage() {
   const navigate = useNavigate();
 
   return (
-    <>
-      <h1>수강신청 사이트</h1>
-      <Button onClick={() => navigate(ROUTE_PATH.SIGN_UP)}>
-        회원가입 하기
+    <S.Container>
+      <S.Title>월부 수강신청</S.Title>
+      <S.Description>
+        안녕하세요, 월급쟁이부자들 FE 에 지원한 진나영입니다😊 <br />
+        과제 기회를 주셔서 감사합니다. 과제 사이트 제출합니다!
+      </S.Description>
+      <Button onClick={() => navigate(ROUTE_PATH.LOGIN)}>
+        이메일로 시작하기
       </Button>
-    </>
+    </S.Container>
   );
 }
 
 export default MainPage;
+
+const S = {
+  Container: styled.div`
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: ${({ theme }) => theme.GAP.level4};
+
+    padding: ${({ theme }) => theme.PADDING.p8};
+  `,
+  Title: styled.h1`
+    color: ${({ theme }) => theme.PALETTE.gray[100]};
+    font: ${({ theme }) => theme.FONTS.heading.small};
+  `,
+  Description: styled.p`
+    color: ${({ theme }) => theme.PALETTE.gray[70]};
+    font: ${({ theme }) => theme.FONTS.body.medium};
+  `,
+};
