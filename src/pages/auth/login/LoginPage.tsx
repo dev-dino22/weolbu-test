c8d59dfe9eb1e4b494ca740d76300bf09eb96ed5
dev-dino22 <wlsskdud02@naver.com>
2025-12-15 17:13:07 +0900

    chore: 주석 삭제

--- src/pages/auth/login/LoginPage.tsx
+++ src/pages/auth/login/LoginPage.tsx
@@ -1,5 +1,5 @@
-import styled from '@emotion/styled';
 import LoginForm from '@domains/auth/components/LoginForm';
+import styled from '@emotion/styled';
 
 function LoginPage() {
   return (
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,7 +5,17 @@ import { ROUTE_PATH } from '@routes/routePath';
 import { THEME } from '@styles/global';
 import reset from '@styles/reset';
 
+import { queryClient } from '@apis/queryClient';
+import ToastProvider from '@components/toast/ToastProvider';
+import { AuthProvider, useAuth } from '@domains/auth/context/AuthProvider';
 import { Global, ThemeProvider } from '@emotion/react';
+import LoginPage from '@pages/auth/login/LoginPage';
+import SignUpPage from '@pages/auth/signUp/SignUpPage';
+import CourseCreatePage from '@pages/course/CourseCreatePage';
+import CourseEnrollPage from '@pages/course/CourseEnrollPage';
+import Main from '@pages/main/MainPage';
+import { QueryClientProvider } from '@tanstack/react-query';
+import Header from '@widgets/Header';
 import {
   createBrowserRouter,
   Navigate,
@@ -13,17 +23,6 @@ import {
   RouterProvider,
   useLocation,
 } from 'react-router';
-import Main from '@pages/main/MainPage';
-import SignUpPage from '@pages/auth/signUp/SignUpPage';
-import ToastProvider from '@components/toast/ToastProvider';
-import LoginPage from '@pages/auth/login/LoginPage';
-import { AuthProvider, useAuth } from '@domains/auth/context/AuthProvider';
-import CourseCreatePage from '@pages/course/CourseCreatePage';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@apis/queryClient';
-import CourseEnrollPage from '@pages/course/CourseEnrollPage';
-import Header from '@widgets/Header';
-import { useShowToast } from '@components/toast/ToastProvider';
 
 function Wrapper() {
   return (
@@ -48,16 +47,17 @@ function Wrapper() {
 function ProtectedRoute() {
   const { loggedIn, loading, hasToken, logoutUser } = useAuth();
   const location = useLocation();
-  const showToast = useShowToast();
 
   if (loading) return null;
 
   if (!loggedIn || !hasToken()) {
-    // TODO: 로그아웃할 때 토스트 두번 뜨는 거 수정
-    showToast({ mode: 'WARN', message: '로그인이 필요합니다.' });
     logoutUser();
     return (
-      <Navigate to={ROUTE_PATH.LOGIN} state={{ from: location }} replace />
+      <Navigate
+        to={ROUTE_PATH.LOGIN}
+        state={{ from: location, needsAuth: true }}
+        replace
+      />
     );
   }
 
